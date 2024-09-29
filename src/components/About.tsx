a69ef3795ce3bebd4539bdcf026c1ce10a827180
HarshilTomar20 <harshiltomar20@gmail.com>
2024-09-29 14:25:00 +0530

    feat: pic integrate

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,10 +6,11 @@ import { motion } from "framer-motion";
 
 export default function About() {
   const images = [
-    "https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    "/images/me/5.jpeg",
+    "/images/me/3.jpeg",
+    "/images/me/6.jpeg",
+    "/images/me/7.jpeg",
+
   ];
 
   return (
@@ -43,7 +44,7 @@ export default function About() {
 
       <div className="max-w-4xl mx-auto">
         <Paragraph className="mt-4">
-          Hello there! I&apos;m Harshil Tomar, a passionate developer, avid writer, and design enthusiast. Welcome to my digital world!
+          Hello there! I&apos;m Harshil Tomar, a passionate developer, avid reader, and design enthusiast. Welcome to my digital world!
         </Paragraph>
 
         <h2 className="text-2xl font-semibold mt-6 mb-3">Professional Journey</h2>
