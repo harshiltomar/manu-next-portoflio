0ed2a17729d987aa2415c6c25129ba8a285ea903
HarshilTomar20 <harshiltomar20@gmail.com>
2024-09-29 11:23:08 +0530

    feat: logo updates

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,7 +106,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src="https://avatars.githubusercontent.com/u/110554721?v=4"
         alt="Avatar"
         height="40"
         width="40"
